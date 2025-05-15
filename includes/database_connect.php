fa1fea4923023d3f75fb4236964f645d4091f2a7
Keshabkjha <keshabkumarjha876@gmail.com>
2025-05-15 12:32:39 +0530

    UPDATED

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("127.0.0.1", "root", "", "pglife");
+    $conn = mysqli_connect("localhost:3307", "root", "", "pglife");
     
     if(mysqli_connect_errno()){
         echo "Connection Error: ".mysqli_connect_error();
